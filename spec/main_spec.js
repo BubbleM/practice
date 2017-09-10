d22d8b96a39ca09979f1385e627de4dece308853
BubbleM <1870408473@qq.com>
2017-09-10 20:53:11 +0800

    to extract the parameters

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -18,7 +18,8 @@ describe("测试描述", function(){
       let order2 = 'U231 2016-06-02 10:00~12:00 A';
       let order3 = 'U213 2016-06-03 20:00~22:00 A';
       let order4 = 'U311 2016-06-04 09:00~10:00 B';
-      var result = main();
+      let orders = [order1,order2,order3,order4];
+      var result = main(orders);
       var expect_string = '';
         
       expect(result).to.equal(`收⼊汇总
@@ -38,6 +39,35 @@ describe("测试描述", function(){
 ---
 总计: 250元`);
     });
+
+  xit("测试用例1", function(){
+
+    let order1 = 'U001 2016-06-02 22:00~22:00 A';
+    let order2 = 'U002 2017-08-01 19:00~22:00 A';
+    let order3 = 'U003 2017-08-02 13:00~17:00 B';
+    let order4 = 'U004 2017-08-03 15:00~16:00 C';
+    let order5 = 'U005 2017-08-05 09:00~11:00 D';
+    let orders = [order1,order2,order3,order4,order5];
+    var result = main(orders);
+    var expect_string = '';
+
+    expect(result).to.equal(`收⼊汇总
+---
+场地:A
+2016-06-02 09:00~10:00 30元
+2016-06-02 10:00~12:00 60元
+2016-06-03 20:00~22:00 120元
+⼩计：210元
+场地:B
+2016-06-04 09:00~10:00 40元
+⼩计：40元
+场地:C
+⼩计：0元
+场地:D
+⼩计：0元
+---
+总计: 250元`);
+  });
 
   xit("取消预约扣除违约金收入测试", function(){
 
