c668dccf7a4e1d268ff9a38b97b1b0ef1d0a7f6f
BubbleM <1870408473@qq.com>
2017-09-11 15:01:00 +0800

    check project

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,36 +8,39 @@ chai.use(sinonChai);
 
 var main = require("../lib/main.js");
 
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
+describe("测试描述", function () {
+  sinon.spy(console, 'log');
 
-    it("收入汇总测试", function(){
-      let order1 = 'U123 2016-06-02 09:00~10:00 A';
-      let order2 = 'U231 2016-06-02 10:00~12:00 A';
-      let order3 = 'U213 2016-06-03 20:00~22:00 A';
-      let order4 = 'U311 2016-06-04 09:00~10:00 B';
-      let orders = [order1,order2,order3,order4];
-      var result = main(orders);
+  it("收入汇总测试", function () {
+    let order1 = 'U123 2016-06-02 09:00~10:00 A';
+    let order2 = 'U231 2016-06-02 10:00~12:00 A';
+    let order3 = 'U213 2016-06-03 20:00~22:00 A';
+    let order4 = 'U311 2016-06-04 09:00~10:00 B';
+    let orders = [order1, order2, order3, order4];
+    var result = main(orders);
 
-      expect(result).to.equal(`收⼊汇总
+    expect(result).to.equal(`收⼊汇总
 ---
 场地:A
 2016-06-02 09:00~10:00 30元
 2016-06-02 10:00~12:00 60元
 2016-06-03 20:00~22:00 120元
 ⼩计：210元
+
 场地:B
 2016-06-04 09:00~10:00 40元
 ⼩计：40元
+
 场地:C
 ⼩计：0元
+
 场地:D
 ⼩计：0元
 ---
 总计: 250元`);
-    });
+  });
 
-  it("取消预约扣除违约金收入测试", function(){
+  it("取消预约扣除违约金收入测试", function () {
     let order1 = 'U123 2016-06-02 09:00~10:00 A';
     let order2 = 'U231 2016-06-02 10:00~12:00 A';
     let order3 = 'U213 2016-06-03 20:00~22:00 A';
@@ -53,24 +56,27 @@ describe("测试描述", function(){
 2016-06-02 10:00~12:00 60元
 2016-06-03 20:00~22:00 120元
 ⼩计：195元
+
 场地:B
 2016-06-04 09:00~10:00 40元
 ⼩计：40元
+
 场地:C
 ⼩计：0元
+
 场地:D
 ⼩计：0元
 ---
 总计: 235元`);
   });
 
-  it("测试用例1", function(){
+  it("测试用例1", function () {
     let order1 = 'U001 2016-06-02 22:00~22:00 A';
     let order2 = 'U002 2017-08-01 19:00~22:00 A';
     let order3 = 'U003 2017-08-02 13:00~17:00 B';
     let order4 = 'U004 2017-08-03 15:00~16:00 C';
     let order5 = 'U005 2017-08-05 09:00~11:00 D';
-    let orders = [order1,order2,order3,order4,order5];
+    let orders = [order1, order2, order3, order4, order5];
     var result = main(orders);
 
     expect(result).to.equal(`收⼊汇总
@@ -78,12 +84,15 @@ describe("测试描述", function(){
 场地:A
 2017-08-01 19:00~22:00 200元
 ⼩计：200元
+
 场地:B
 2017-08-02 13:00~17:00 200元
 ⼩计：200元
+
 场地:C
 2017-08-03 15:00~16:00 50元
 ⼩计：50元
+
 场地:D
 2017-08-05 09:00~11:00 80元
 ⼩计：80元
@@ -91,14 +100,14 @@ describe("测试描述", function(){
 总计: 530元`);
   });
 
-  it("测试用例2", function(){
+  it("测试用例2", function () {
     let order1 = 'U002 2017-08-01 19:00~22:00 A';
     let order2 = 'U003 2017-08-01 18:00~20:00 A';
     let order3 = 'U002 2017-08-01 19:00~22:00 A C';
     let order4 = 'U002 2017-08-01 19:00~22:00 A C';
     let order5 = 'U003 2017-08-01 18:00~20:00 A';
     let order6 = 'U003 2017-08-02 13:00~17:00 B';
-    let orders = [order1,order2,order3,order4,order5,order6];
+    let orders = [order1, order2, order3, order4, order5, order6];
     var result = main(orders);
 
     expect(result).to.equal(`收⼊汇总
@@ -107,11 +116,14 @@ describe("测试描述", function(){
 2017-08-01 19:00~22:00 违约金 100元
 2017-08-01 18:00~20:00 160元
 ⼩计：260元
+
 场地:B
 2017-08-02 13:00~17:00 200元
 ⼩计：200元
+
 场地:C
 ⼩计：0元
+
 场地:D
 ⼩计：0元
 ---
